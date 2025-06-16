986a85e5d7a547ffa2e1526601142b2faf74f2e6
YounYoungSeon <dbsyys@s3i.co.kr>
2025-06-16 18:03:09 +0900

    layout 생성
    tailwind css 추가

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
-  
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: 'navbar', loadComponent: () => import('./layout/navbar/navbar.component').then(m => m.NavbarComponent) },
+            { path: 'sidebar', loadComponent: () => import('./layout/sidebar/sidebar.component').then(m => m.SidebarComponent) },
+        ]
+    }
 ];
